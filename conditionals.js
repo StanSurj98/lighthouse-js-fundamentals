3c8724dea864469cc17cfe2d9f627713e054c8cd
Stanley Surjanto <stan.surjanto@gmail.com>
2022-07-30 21:22:33 -0600

    updated conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -55,3 +55,18 @@ if (temperature < -40 || temperature > 40) { // checks to see if EITHER conditio
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+//whichSchool conditionals exercise on Compass
+
+const whichSchool = function (age) {
+  if (age < 13) {
+    return "Elementary School"; // important to use RETURN keyword to then return what it evaluates
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School"; // if we just did console.log("Secondary School"), it would return nothing but only reach our developer console
+  } else {
+    return "Lighthouse Labs"; // the reason being is that on a live site we would get undefined but on the developer console we would see the log
+  }
+}
+console.log("I am 14, which school should I go to?")
+whichSchool(14); // this is correct and would calculate the function and return what we want, but it won't show on our console
+console.log(whichSchool(14)); // this allows us to SEE the word "Secondary School" on our developer console only
