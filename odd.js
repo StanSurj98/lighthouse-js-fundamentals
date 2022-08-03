e74e5719255dc4c60c4902aa72a5171e43e12941
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-03 14:11:46 -0700

    this is a function to check if a number is odd

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd = function (num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(11)); // true
+console.log(isOdd(8)); // false
+console.log("3 is odd: " + isOdd(3)); // true
+console.log("10 is odd: " + isOdd(10)); // false
+
+
+// commit and push to github repo with wifi - aug 2, 2022, 2:21pm
