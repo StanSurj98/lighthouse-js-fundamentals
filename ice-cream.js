2722d028c78e5aa00107fd4dfa123b6e28a33f8d
Stanley Surjanto <stan.surjanto@gmail.com>
2022-08-07 21:26:41 -0600

    ice cream flavors in an array and how to access their elements

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+// first flavor
+console.log(iceCreamFlavours[0]);
+// last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+// total number of elements?
+console.log(iceCreamFlavours.length);
---
+++ x-marks-the-perfect-shot.js
@@ -0,0 +1,15 @@
+// create a function that takes coordinated directions in the form of 
+// an array of strings
+// directions will move us in a [x,y] grid of 1x1
+// the output will return an array representing our final location in a coordinate of [x,y]
+
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+
+const finalPosition = function (moves) {
+  const position = [0, 0];
+  
+
+  return finalCoord;
+}
+
+
